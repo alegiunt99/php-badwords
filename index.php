5e79897df80bcca655181b0acbf8d12fec6c7884
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-03 10:33:39 +0200

    Ho fatto la prima milestone

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$textOne = 'lorem ipsum diet sok ipest ucisar lam fux lames nieap das die dum ';
+
+// echo($textOne);
+
+?>
+
+<h1> TESTO NORMALE </h1>
+
+<p>
+    <?php echo($textOne); ?>
+</p>
+<span>
+    La frase contiene <?php echo(strlen($textOne)); ?> caratteri
+</span>
